757b46bdf95eedc389e6a083f401b8e4ee18e9b7
Reness <reness9910@gmail.com>
2024-07-23 16:56:59 +0800

    Your commit message

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,9 +1,5 @@
-@echo off
+#!/usr/bin/env sh
 
-echo The following command terminates the "npm start" process using its PID
-echo (written to ".pidfile"), all of which were conducted when "deliver.bat"
-echo was executed.
-echo on
-set /p PID=<.pidfile
-taskkill /F /PID %PID%
-echo off
+set -x
+docker kill my-flask-app
+docker rm my-flask-app
